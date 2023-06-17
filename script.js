a71bcf864c061e1e83f95b72b11cafeb933b24e0
rucha-kulkarni <rucha3702@gmail.com>
2023-06-17 23:49:15 +0530

    third commit

--- script.js
+++ script.js
@@ -35,9 +35,9 @@ function removeAdmin() {
 function concatenateArray() {
   //Write your code here, just console.log
   let arr2 = [
-    { id: 4, name: "jems", age: "21", profession: "admin" },
-    { id: 5, name: "cris", age: "17", profession: "developer" },
-    { id: 6, name: "ron", age: "20", profession: "admin" },
+    { id: 5, name: "jems", age: "21", profession: "admin" },
+    { id: 6, name: "cris", age: "17", profession: "developer" },
+    { id: 7, name: "ron", age: "20", profession: "admin" },
   ];
   let newArray = arr.concat(arr2);
   console.log(newArray);
